89152967a9fbbd9e3ea8336852422b70a0cfafef
Leandro Arakaki <lean_ariel@hotmail.com>
2022-02-21 10:49:19 -0300

    subiendo archivos proyecto final

---
+++ js/app.js
@@ -0,0 +1,87 @@
+particlesJS({
+    "particles": {
+      "number": {
+        "value": 128,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#f0f8ff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+    
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 4.008530152163807,
+        "random": true,
+        "anim": {
+          "speed": 40,
+          "size_min": 0.1,
+
+        }
+      },
+      "line_linked": {
+        "enable": false,
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "bottom",
+        "out_mode": "out",
+        "attract": {
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+       
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 0.5
+          }
+        },
+        "bubble": {
+          "distance": 462.88401101176675,
+          "size": 4.060386061506726,
+          "duration": 10,
+          "opacity": 0.04060386061506725,
+          "speed": 3
+        },
+      
+        
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+  );
